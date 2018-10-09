a36f3d811034dee91e01d0b7b609be7d30835e6c
Choccy <faiizow@gmail.com>
2018-10-09 20:05:37 +0100

    more stuff

---
+++ example.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  CHANNEL: '', // channel name
+  BOT_NAME:'', // bot name
+  CHANNELID:'', // your channels ID
+  CLIENTID:'', // you client ID
+  BOT_OAUTH:'', // use https://twitchapps.com/tokengen/
+  OWNER_OAUTH:'', // use https://twitchapps.com/tmi/
+  STREAMLABS_SOCKET_TOKEN:'' // get from https://streamlabs.com/dashboard#/apisettings
+}
